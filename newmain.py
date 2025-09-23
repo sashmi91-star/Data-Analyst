eeb78e03b1fbbc47f63eae3de9a0c3556b3f3094
sashmi.91 <sashmi.91@gmail.com>
2025-09-23 22:20:04 +0530

    first file

---
+++ newmain.py
@@ -0,0 +1,7 @@
+#input the name from users
+name=str(input("Enter your name: "))
+age=int(input("Enter your age: "))
+gender=str(input("Enter your gender: "))
+
+#display to the user
+print(f"Hello, {name} your age is {age} and your gender is {gender}")
